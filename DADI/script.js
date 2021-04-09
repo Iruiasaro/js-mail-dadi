5af65062621d4e446f281195d88e2ca84eef62c4
Iruiasaro <brunettirosario@gmail.com>
2021-04-09 17:28:48 +0200

    variabili e math random per lancio dadi

---
+++ DADI/script.js
@@ -0,0 +1,12 @@
+
+//inserimento variabili e math random
+var JohnnyDado;
+var FrankieDado;
+
+JohnnyDado= Math.floor(Math.random()*6+1);
+console.log("Johnny lancia il dado ed esce il numero: " + JohnnyDado);
+
+FrankieDado= Math.floor(Math.random()*6+1);
+console.log("Frankie lancia il dado ed esce il numero: " + FrankieDado);
+
+
